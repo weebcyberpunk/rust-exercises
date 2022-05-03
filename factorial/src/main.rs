f89f52997625e497bc9185713d26a365aa757378
GG <weebcyberpunk@gmail.com>
2022-05-02 23:41:50 -0300

    Added factorial

---
+++ factorial/src/main.rs
@@ -0,0 +1,36 @@
+use std::io;
+
+fn factorial(mut num: u64) -> u64 {
+
+    let mut r = num;
+
+    loop {
+        r = r * (num - 1);
+        num = num - 1;
+        // TODO learn control flow
+    }
+}
+
+fn main() {
+
+    let mut var = String::new();
+    let num: u64;
+
+    loop {
+        io::stdin()
+            .read_line(&mut var)
+            .expect("Failed to receive input.");
+
+        num = match var.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number!");
+                continue;
+            }
+        };
+
+        break;
+    }
+
+    println!("Factorial: {}", factorial(num));
+}
